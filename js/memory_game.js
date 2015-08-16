7ddf9f81b3f6dac0fcc79b57911dd85bd4746e1d
rroger <rruettimann@rroger-imac.local>
2015-08-17 00:08:55 +0200

    Make game configurable: choose number of symbols and fadeout time. change layout depending on size.

--- js/memory_game.js
+++ js/memory_game.js
@@ -2,21 +2,90 @@
  * Created by rruettimann on 14.08.15.
  */
 
-var GAME_VALUES = ['a','a','a','b','b','b','c','c','c']
+var game_values_template = ['a','a','a','b','b','b','c','c','c']
 
 function init_game() {
-  generate_board();
-  generate_game_values();
+  var board_size = Number($('#board_size').val());
+  var fadeout_duration = Number($('#fadeout_duration').val());
+  console.log(fadeout_duration);
+  if ( ! ( ( board_size >= 2 ) && ( board_size <= 26 ) ) ) {
+    alert("Please type a game size >= 2!");
+    return;
+  }
+  if ( ! ( fadeout_duration >= 0.4 ) ) {
+    alert("Please type a fadeout duration >= 0.4 secs!");
+    return;
+  }
+  generate_board(board_size);
+  adapt_style(board_size);
+  clear_value_to_search();
+  generate_game_values(board_size);
   shuffle_game_values();
-  hide_all_cards();
+  hide_all_cards(fadeout_duration*1000);
 }
 
-function generate_board() {
-
+function generate_board(board_size) {
+  // board_size should be an integer >= 2 and <= 26.
+  var board_tbl = $("#board_tbl")
+  board_tbl.html("");
+  for (var i=0; i < board_size; i++) {
+    var row = $('<div id="row' + i + j + '" class="row"></div>');
+    board_tbl.append(row);
+    for (var j=0; j < board_size; j++) {
+      var cell = $('<div id="r' + i + 'c' + j + '" class="cell"></div>');
+      var card = $('<div id="card' + (i*board_size + j) + '" class="card"></div>');
+      cell.append(card);
+      row.append(cell);
+    }
+  }
 }
 
-function generate_game_values() {
+function adapt_style(board_size) {
+  // board_size should be an integer >= 2 and <= 26.
+  jss.set('.cell', {
+    'width': (100/board_size).toString() + '%',
+    'height': (100/board_size).toString() + '%',
 
+  });
+  jss.set('.row', {
+    'width': '100%',
+    'height': (100/board_size).toString() + '%',
+
+  });
+  if (board_size < 7) {
+    jss.set('.table', {
+      'width': '600px',
+      'height': '600px',
+    });
+    jss.set('#end-message', {
+      'top': '100px',
+      'left': '100px',
+      'width': '400px',
+      'height': '400px',
+    });
+  } else {
+    jss.set('.table', {
+      'width': (board_size*100).toString() + 'px',
+      'height': (board_size*100).toString() + 'px',
+    });
+    jss.set('#end-message', {
+      'top': '150px',
+      'left': '150px',
+      'width': (board_size*100 -300).toString() + 'px',
+      'height': (board_size*100 -300).toString() + 'px',
+    });
+  }
+}
+
+function generate_game_values(board_size) {
+  // board_size should be an integer >= 2 and <= 26.
+  game_values_template = [];
+  for (var i = 0; i < board_size; i++) {
+    var sym = String.fromCharCode("a".charCodeAt(0) + i);
+    for (var j = 0; j < board_size; j++) {
+      game_values_template.push(sym);
+    }
+  }
 }
 
 function shuffle(array_) {
@@ -41,6 +110,10 @@ function shuffle(array_) {
     return array;
 }
 
+function clear_value_to_search() {
+  $('#search-value-box').html('&nbsp;');
+}
+
 function gameStep(event) {
     play_sound();
     if ($(event.target).prop('tagName') === 'DIV') {
@@ -54,32 +127,34 @@ function gameStep(event) {
     $('.open-card').each(function () {
         if ($(this).text() != eval_value) {
             $('.card').off('click');
-            $('#board_tbl').prepend('<div class="end-message"><h2><span>Game Over!</span></h2></div>');
+            $('#board_tbl').prepend('<div id="end-message"><h2><span>Game Over!</span></h2></div>');
             $(this).css('background-color','salmon');
             game_over = true;
         }
     })
-    if (($('.open-card').length === 3 ) && (! game_over)) {
+    if (($('.open-card').length === Number($('#board_size').val()) ) && (! game_over)) {
         $('.card').off('click');
-        $('#board_tbl').append('<div class="end-message"><h2><span>You have won!</span></h2></div>');
+        $('#board_tbl').append('<div id="end-message"><h2><span>You have won!</span></h2></div>');
 
     }
 }
 
 function shuffle_game_values(){
-    var game_values = shuffle(GAME_VALUES.slice());
+    var game_values = shuffle(game_values_template.slice());
     $('.card').each(function( index ) {
         //$(this).text('test');
         $(this).html('<span>' + game_values.pop() + '</span>');
     })
 }
 
-function get_random_card_value() {
+function get_random_card_value(board_size) {
     var rand = Math.random();
-
-    if (rand < 0.33) return 'a';
-    if (rand >=  0.66) return 'b';
-    return 'c';
+    var board_size = Number($('#board_size').val());
+    for (var i = 0; i < board_size; i++) {
+      if (rand < ((i+1)/board_size)){
+        return String.fromCharCode("a".charCodeAt(0) + i);
+      }
+    }
 }
 
 function hide_all_cards(fadeout_duration) {
